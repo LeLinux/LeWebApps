7f55de4cf05d2c7f6f52cf7270615c9e7b4769ae
LeLinux <lelinuxgit@gmail.com>
2023-07-19 17:02:57 +0300

    readme

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import subprocess as sb
-sb.call(["sudo", "apt-get", "install", "python3-pip"])
+sb.call(["sudo", "apt-get", "install", "python3-pip", "surf"])
 sb.call(["pip3", "install", "pyqtdarktheme"])
 sb.call(["pip3", "install", "pyside6"])
 sb.call(["pip3", "install", "PySide6-Fluent-Widgets", "-i" , "https://pypi.org/simple/"])
